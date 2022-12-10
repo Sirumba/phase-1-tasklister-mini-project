a416159de5318dbf7681699313035eb9db71bb2d
Sirumba <simonsmaina45@gmail.com>
2022-12-10 21:35:31 +0300

    Changes made for resubmission

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-  document.addEventListener("DOMContentLoaded", () => {
-    // your code here
-    document.getElementById("craete-task-form").addEventListener("submit", (e) =>{
-      e.preventDefault()
-      submitBtn(e.target.input.value)
-    })
-  });
-  function submitBtn(task){
-    let ul = document.getElementById('tasks')
-    let li = document.createElement("li")
-    ul.appendChild(li)
-    li.inerText =`${task}`
-    let x = document.createElement('button')
-    x.addEventListener("click", deleteElem)
-    x.innerText= 'X'
-    li.appendChild(x)
-  }
-  function deleteElem(e){
-    e.target.parentNode.remove()
-  }
+document.querySelector("form").addEventListener('submit', (e) => {
+  e.preventDefault();
+  tasks(e.target.value)
+  document.querySelector('form').reset();
 });
+
+function tasks(task) {
+const input = document.getElementById("new-task-description");
+let li = document.createElement("li");
+let btn = document.createElement("button")
+btn.addEventListener('click', removeTask)
+btn.textContent = 'X';
+li.innerHTML = input.value;
+li.appendChild(btn)
+document.querySelector("#tasks").appendChild(li);
+}
+function removeTask(e) {
+e.target.parentNode.remove();
+}
